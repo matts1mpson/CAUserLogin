49b8228c339b28fc575e06f6f2fe84234a112562
Matty Strider <mattystrider@Mattys-MacBook-Air.local>
2024-10-25 18:12:53 -0400

    Task 2.2 complete.

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name of the current user
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return Returns the current user.
+     */
+    String getCurrentUser();
 }
